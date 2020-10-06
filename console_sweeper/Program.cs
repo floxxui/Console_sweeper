662f81c1691a11d9f841ae21e7e651cdf72d6a47
floxxui <60339661+floxxui@users.noreply.github.com>
2020-10-06 12:37:35 +0200

    commit start

---
+++ console_sweeper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console_sweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To do: ");
+            System.Console.WriteLine("Completed: ");
+        }
+    }
+}
